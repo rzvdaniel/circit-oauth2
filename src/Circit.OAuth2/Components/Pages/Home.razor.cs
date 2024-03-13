4ab25e4eeeda243a2bf455eb28b23855e29459fd
rzvdaniel <rzvdaniel@gmail.com>
2024-03-13 22:11:31 +0100

    Bootstrap a simple blazor web app

---
+++ src/Circit.OAuth2/Components/Pages/Home.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Circit.OAuth2.Components.Pages
+{
+    public partial class Home : ComponentBase
+    {
+
+    }
+}
---
+++ src/Circit.OAuth2/Components/Pages/Profile.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Circit.OAuth2.Components.Pages
+{
+    public partial class Home : ComponentBase
+    {
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+        }
+    }
+}
