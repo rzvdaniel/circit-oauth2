fe911693ab75ae4c053801411e5bec2d07e00306
rzvdaniel <rzvdaniel@gmail.com>
2024-03-14 19:12:15 +0100

    Added small unit test for UserProfile

---
+++ src/Circit.OAuth2.Tests/UserProfileTests.cs
@@ -0,0 +1,27 @@
+﻿using Circit.OAuth2.Models;
+
+namespace Circit.OAuth2.Tests
+{
+    [TestClass]
+    public class UserProfileTests
+    {
+        [TestMethod]
+        public void ValidUserProfile()
+        {
+            var userProfile = new UserProfile
+            {
+                Login = "test@gmail.com"
+            };
+
+            Assert.IsTrue(userProfile.IsValid);
+        }
+
+        [TestMethod]
+        public void invalidUserProfile()
+        {
+            var userProfile = new UserProfile();
+
+            Assert.IsFalse(userProfile.IsValid);
+        }
+    }
+}
